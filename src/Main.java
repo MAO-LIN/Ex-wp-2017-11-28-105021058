9b2b66c62f7e1428befd34beccfb84fa23a1498e
SamaLin <105021058@live.asia.edu.tw>
2017-12-07 13:02:37 +0800

    jack

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,6 @@
-import com.sun.org.apache.xpath.internal.SourceTree;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-//        ServerFrame serF =new ServerFrame();
-//        serF.setVisible(true);
-//        ClientFrame cliF =new ClientFrame();
-//        cliF.setVisible(true);
-        GameFrame gF=new GameFrame();
-        gF.setVisible(true);
-
-//        try{
-//            InetAddress adr=InetAddress.getLocalHost();
-//            System.out.println(adr.getHostAddress());
-//
-//        }catch(UnknownHostException e){
-//            System.out.println("");
-//        }
     }
 }
